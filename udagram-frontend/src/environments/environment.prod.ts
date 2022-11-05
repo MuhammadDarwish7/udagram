59707200874a445ae54975b711d50ce0fb4abade
MuhammadDarwish7 <muhammadalydarwish@gmail.com>
2022-11-06 01:03:49 +0200

    version 28

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://af00112e6cd1d413bb0e3e7e7faff37c-405072900.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
